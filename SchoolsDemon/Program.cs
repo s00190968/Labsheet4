fd9e3da8a68557c5ced5719e1210fda836539c59
Janessa Aulén <43206084+s00190968@users.noreply.github.com>
2018-11-07 17:02:21 +0000

    Add files via upload
    Added school class and inouts for user schoolos aren't being sorted into right order yet.

---
+++ SchoolsDemon/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int num = 0;
+
+            School[] AllSchools = new School[5];
+
+            Console.WriteLine("Enter data for five schools.");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + ". Enter school's name:");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Enter the amount of students enrolled.");
+                while (!int.TryParse(Console.ReadLine(), out num))
+                {
+                    Console.WriteLine("Enter the amount of students enrolled.");
+                }
+
+                AllSchools[i] = new School(name, num);
+            }
+
+            displaySchools(AllSchools);
+
+            Array.Sort(AllSchools);
+
+            displaySchools(AllSchools);
+
+        }
+
+        public static void displaySchools(School[] arr)
+        {
+            Console.WriteLine(" All schools:\n");
+            foreach (School s in arr)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SchoolsDemon/School.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDemon
+{
+    class School : IComparable
+    {
+        public string schoolName { get; }
+        public int numOfStudents { get; set; }
+
+        DateTime lastEdited;
+
+        public School(string name, int number)
+        {
+            this.schoolName = name;
+            this.numOfStudents = number;
+            lastEdited = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\tName: {0}, students enrolled: {1}.", schoolName, numOfStudents);
+        }
+        public int CompareTo(object obj)
+        {
+            School other = (School)obj;
+
+            if (this.lastEdited > other.lastEdited)
+            {
+                return 1;
+            }else if(this.lastEdited < other.lastEdited)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
